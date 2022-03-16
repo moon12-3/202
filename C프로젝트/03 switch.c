f043c250c59e8c9c5746191eb1f0ee693289e714
chaeyoung moon <cymoon05@naver.com>
2022-03-16 10:17:51 +0900

    switch break 안함

---
+++ C프로젝트/03 switch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int x = 1;
+	// break X, 12d Ãâ·Â
+	switch (x) {
+	case 0 :
+		printf("0");
+	case 1 : 
+		printf("1");
+	case 2 : 
+		printf("2");
+	default : 
+		printf("d");
+	
+	}
+
+	return 0;
+}

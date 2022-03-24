1f923821b2312b46af4dd52f131cd4de157db0ae
chaeyoung moon <cymoon05@naver.com>
2022-03-24 10:51:06 +0900

    17 증감연산자

---
+++ 17 증감연산자.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+	int a, b, c = 0;	//이러면 c만 0
+	// c가 먼저 1증가하고 a에 대입, c:1, a:1
+	a = ++c;
+	// b에 c값을 먼저 대입하고 c가 1증가. b:1, c:2
+	b = c++;
+
+	printf("%d %d %d", a, b, ++c);
+
+	return 0;
+}

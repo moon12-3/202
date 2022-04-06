dfcac753fa7de5ce86e81667fd865abec3350e8a
chaeyoung moon <cymoon05@naver.com>
2022-04-06 09:08:45 +0900

    매크로 함수 완성

--- C++기본/02-01 매크로함수.cpp
+++ C++기본/02-01 매크로함수.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 // 자료형에 영향 X
-#define jegob(A)	A*A
+#define jegob(A)	((A)*(A))
 
 int main() {
 
 	cout << jegob(3+1) << endl;	// 연산자 우선순위 때문에 의도치 않은 결과가 나옴 (7)
-	cout << 3+1 * 3+1 << endl;
+	cout << ((3+1) * (3+1)) << endl;
 	return 0;
 }

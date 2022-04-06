dee9e055700a783e319731aafcbd7574fe3e61e7
chaeyoung moon <cymoon05@naver.com>
2022-04-06 10:06:05 +0900

    call by reference

--- C++기본/02 - 04 call by reference.cpp
+++ C++기본/02 - 04 call by reference.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void plus2(int* num) {
-	*num += 2;
+void plus2(int& num) {
+	num += 2;
 }
 
 int main() {
-	int a = 5;
-	plus2(&a);
+	int a = 3;
+	plus2(a);
 	cout << a << endl;	//5
 
 	return 0;

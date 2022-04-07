5964446155c520f67db413bf397d5604175da5ba
chaeyoung moon <cymoon05@naver.com>
2022-04-07 11:38:43 +0900

    string 덧붙이기

---
+++ 02 - 07 string 덧붙이기.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>	//strcpy()를 가지고 있음
+#include<string>	//string 클래스를 가지고 있음
+
+using namespace std;
+
+int main() {
+	char cMunja[20];
+	strcpy(cMunja, "c_insert");	//c_insert
+	strcat(cMunja, "++");		//c_insert++
+	printf("%s\n", cMunja);
+
+	std::string cppMunja = "cpp_insert";	//cpp_insert
+	cppMunja += "++";						//cpp_insert++
+	cout << cppMunja << endl;
+	return 0;
+}

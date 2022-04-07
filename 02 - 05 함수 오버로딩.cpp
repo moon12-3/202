e97fce2b1e79a2734b62fd7dff54a60bee61f5fd
chaeyoung moon <cymoon05@naver.com>
2022-04-07 10:59:28 +0900

    default 매개변수(함수 오버로딩과는 별개)

--- 02 - 05 함수 오버로딩.cpp
+++ 02 - 05 함수 오버로딩.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 // 함수 오버로딩 (함수 이름은 같고, 매개변수는 다름)
-int jegob(int A, int B) {
+int jegob(int A, int B=1) {
 	return A * A + B;
 }
 
-double jegob(double A, double B) {
+double jegob(double A, double B=1.1) {
 	return A * A + B;
 }
 
 int main() {
-	cout << jegob(4, 1) << endl;
-	cout << jegob(3.3, 1.1) << endl;
+	cout << jegob(4) << endl;	//17
+	cout << jegob(3.3) << endl;	//11.99
 	
 	return 0;
 }

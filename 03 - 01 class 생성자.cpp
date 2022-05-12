dc1b734e7049c84e7c369e7329992fac4c44f616
chaeyoung moon <cymoon05@naver.com>
2022-05-12 12:24:58 +0900

    소멸자 추가

--- 03 - 01 class 생성자.cpp
+++ 03 - 01 class 생성자.cpp
@@ -1,50 +1,49 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string.h> 
 
 using namespace std;
 
-class Student {
-private :
-	int iStuNum;
-	string strName;
+class Student
+{
+private:
+	int nHakbun;
+	char* sName;
 
-	// 생성자 : 객체가 생성될 때 호출되는 함수. 반환형을 쓰지 않는다.
-	// 디폴트 생성자를 컴파일러가 만들어준다.
-public :
-	Student();
-	Student(int, string);
+public:
+	// 생성자 : 객체가 생성될 때, 호출되는 함수
+	Student(int Hakbun, const char* Name);
+	// 소멸자 : 객체가 메모리에서 해제될 때, 호출되는 함수
+	~Student();
 
 	void show(void);
-	
 };
 
-int main() {
-	Student* stu = new Student[6];
-	for (int i = 0; i < 6; i++)
-		stu[i].show();
-		// 배열이므로 ->를 쓰지 않고 .으로 멤버접근
-	delete []stu;
+int main(void) {
+	Student stu1 = Student(1111, "JWP");
+	stu1.show();
 
 	return 0;
 }
 
-Student::Student() {
-	iStuNum = 1234;
-	strName = "이사랑";
-	cout << "학번이 등록되었습니다." << endl;
-}
-
-Student::Student(int StuNum, string sname)
-// 객체 생성 동시에 멤버 변수 초기화	// 멤버변수(매개변수)
-// const와 참조형 멤버변수를 사용할 수 있다.
-	: iStuNum(StuNum), strName(sname) 
+Student::Student(int Hakbun, const char* Name)
+// 객체 생성과 동시에 멤버변수 초기화   // 멤버변수(매개변수)
+// const/참조형 멤버변수를 사용할 수 있다.
+	: nHakbun(Hakbun)
 {
-	
-	cout << "학번이 등록되었습니다." << endl;
+	cout << "일반생성자 호출" << endl;
+	this->nHakbun = Hakbun;
+	int len = strlen(Name) + 1; // 동적할당할 공간의 크기를 구하고
+	sName = new char[len];		// 그 크기만큼 배열로 동적할당
+	strcpy(sName, Name);		// 매개변수에 있는 문자열 복사
+};
 
+Student::~Student()
+{
+	delete[]sName;
+	cout << "소멸자 호출" << endl;
 }
 
 void Student::show(void) {
-	cout << "학번은 " << iStuNum << "입니다." << endl;
-	cout << "이름은 " << strName << "입니다." << endl << endl;
+	cout << "학번은 " << nHakbun << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;;
 }

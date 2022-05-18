b3783ed814aba9c5fdb3eb159d12ba0fbc9abb05
chaeyoung moon <cymoon05@naver.com>
2022-05-18 09:01:16 +0900

    string을 char*형식으로 전환

--- 03 - 01 class 생성자.cpp
+++ 03 - 01 class 생성자.cpp
@@ -11,7 +11,7 @@ private:
 
 public:
 	// 생성자 : 객체가 생성될 때, 호출되는 함수
-	Student(int Hakbun, const char* Name);
+	Student(int Hakbun, const char* Name);	//const 없으면 오류남
 	// 소멸자 : 객체가 메모리에서 해제될 때, 호출되는 함수
 	~Student();
 
@@ -25,16 +25,14 @@ int main(void) {
 	return 0;
 }
 
-Student::Student(int Hakbun, const char* Name)
+Student::Student(int Hakbun, const char* Name) {
 // 객체 생성과 동시에 멤버변수 초기화   // 멤버변수(매개변수)
 // const/참조형 멤버변수를 사용할 수 있다.
-	: nHakbun(Hakbun)
-{
+	nHakbun = Hakbun;
 	cout << "일반생성자 호출" << endl;
-	this->nHakbun = Hakbun;
-	int len = strlen(Name) + 1; // 동적할당할 공간의 크기를 구하고
-	sName = new char[len];		// 그 크기만큼 배열로 동적할당
-	strcpy(sName, Name);		// 매개변수에 있는 문자열 복사
+	int len = strlen(Name) + 1;	// 공간개수 측정
+	sName = new char[len];
+	strcpy(sName, Name);
 };
 
 Student::~Student()

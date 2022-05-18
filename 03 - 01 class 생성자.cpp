cb9cbd61db08fa5179663681ece358096240ae99
chaeyoung moon <cymoon05@naver.com>
2022-05-18 09:46:46 +0900

    복사생성자(얕은 복사)

--- 03 - 01 class 생성자.cpp
+++ 03 - 01 class 생성자.cpp
@@ -14,13 +14,16 @@ public:
 	Student(int Hakbun, const char* Name);	//const 없으면 오류남
 	// 소멸자 : 객체가 메모리에서 해제될 때, 호출되는 함수
 	~Student();
+	Student(const Student&);
 
 	void show(void);
 };
 
 int main(void) {
-	Student stu1 = Student(1111, "JWP");
+	Student stu1= Student(1111, "JWP");	// 일반 생성자 호출
+	Student stu2 = stu1;	// 복사 생성자 호출
 	stu1.show();
+	stu2.show();
 
 	return 0;
 }
@@ -35,6 +38,12 @@ Student::Student(int Hakbun, const char* Name)
 	sName = new char[len];
 	strcpy(sName, Name);
 };
+
+Student::Student(const Student& rhs)
+	: nHakbun(rhs.nHakbun), sName(rhs.sName)
+{
+	cout << "복사생성자 호출" << endl;
+}
 
 Student::~Student()
 {

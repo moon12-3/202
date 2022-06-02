84313c8561858307305b8ce0429c3b7160ce337a
chaeyoung moon <cymoon05@naver.com>
2022-06-02 11:53:57 +0900

    연산자 오버로딩

--- C++기본/04-01 연산자 오버로딩 기본.cpp
+++ C++기본/04-01 연산자 오버로딩 기본.cpp
@@ -4,6 +4,16 @@ using namespace std;
 class Point {
 public :
 	Point(int x1, int x2);
+	Point();
+	Point operator+(const Point& rhs)	// 바뀌지 않도록 const
+	{
+		Point p;
+		p.mX = mX + rhs.mX;
+		p.mY = mY + rhs.mY;
+
+		return p;
+	}
+
 	void print();
 private :
 	int mX;
@@ -12,12 +22,18 @@ private :
 
 int main() {
 	Point p1(1, 2);
-	p1.print();
+	Point p2(3,4);
+	
+	Point sum = p1 + p2;
+	sum.print();
+
 	return 0;
 }
 
 Point::Point(int x1, int x2)
 	:mX(x1), mY(x2) {}
+
+Point::Point() {}
 
 void Point::print() {
 	cout << "(" << mX << ", " << mY << ")" << endl;
